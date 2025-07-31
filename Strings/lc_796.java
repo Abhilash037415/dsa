6758b7cff4a33c83a87ab996e7dcc656c2526069
Abhilash037415 <abhilashmunnangi12@gmail.com>
2025-07-31 08:28:05 +0530

    Day 4

--- Strings/lc_796.java
+++ Strings/lc_796.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class lc_796{
     public static void main(String args[]){
-        
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        String goal = input.next();
+
+        if(s.length() != goal.length()){
+            System.out.println(false);
+            input.close();
+            return;
+        }
+
+        System.out.println((s+s).contains(goal));
+        input.close();
     }
 }
