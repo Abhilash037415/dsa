8eb6384971a89bed5da676180451593ca4689396
Abhilash037415 <abhilashmunnangi12@gmail.com>
2025-08-24 08:01:04 +0530

    arrays day 3 start

---
+++ Arrays/lc_1572.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class lc_1572 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        int n = input.nextInt();
+        int[][] mat = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                mat[i][j] = input.nextInt();
+            }
+        }
+
+        int sum = 0;
+        for(int i=0;i<mat.length;i++){
+            sum+=mat[i][i];
+        }
+        for(int i=0,j=mat.length-1;i<mat.length;i++,j--){
+            if(i==j)    continue;
+            sum += mat[i][j];
+        }
+
+        System.out.println(sum);
+    }
+}
